a316c3f6667ac39dec4e5bc53706325e4ec9633c
Christian Radaza <cr605@feynman.kent.ac.uk>
2020-12-16 18:05:05 +0000

    Cythonised lib and chi 600/1000 data

---
+++ tenpy/_version.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GENERATED FROM setup.py
+# thus, it contains the version during compilation
+# Copyright 2018-2020 TeNPy Developers, GNU GPLv3
+version = '0.7.2'
+short_version = 'v' + version
+released = False
+full_version = '0.7.2.dev0+9bd660d'
+git_revision = '9bd660de53258c3e850b119bc8e5ad9521015000'
+numpy_version = '1.18.5+intel.5'
+cython_version = '0.29.17'
